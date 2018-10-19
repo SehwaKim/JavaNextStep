a8eb90813fa51ca66a302a1b1ac8daf48a8d8d4d
SehwaKim <sehwakm@gmail.com>
2018-10-19 13:51:36 +0900

    응답 데이터를 처리하는 로직을 별도로 분리한 클래스 HttpResponse에 대한 테스트 코드 작성

--- study01/web-application-server/src/test/java/http/HttpResponseTest.java
+++ study01/web-application-server/src/test/java/http/HttpResponseTest.java
@@ -2,34 +2,36 @@ package http;
 
 import org.junit.Test;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class HttpResponseTest {
-    private String testDirectory = "./src/test/resources/";
+    private String testDirectory = "../web-application-server/src/test/resources/";
 
     @Test
     public void responseForward() throws Exception {
+        // Http_Forward.txt 결과는 응답 body 의 index.html 이 포함되어 있어야 한다.
         HttpResponse response = new HttpResponse(createOutputStream("Http_Forward.txt"));
         response.forward("/index.html");
     }
 
     @Test
     public void responseRedirect() throws Exception {
+        // Http_Redirect.txt 결과는 응답 header 에
+        // Location 정보가 /index.html 로 포함되어 있어야 한다.
         HttpResponse response = new HttpResponse(createOutputStream("Http_Redirect.txt"));
         response.sendRedirect("/index.html");
     }
 
     @Test
     public void responseCookies() throws Exception {
+        // Http_Cookie.txt 결과는 응답 header 에 Set-Cookie 값으로
+        // logined=true 값이 포함되어 있어야 한다.
         HttpResponse response = new HttpResponse(createOutputStream("Http_Cookie.txt"));
         response.addHeader("Set-Cookie", "logined=true");
         response.sendRedirect("/index.html");
     }
 
-    private OutputStream createOutputStream(String fileName) throws FileNotFoundException {
-        return new FileOutputStream(new File(testDirectory + fileName));
+    private OutputStream createOutputStream(String filename) throws FileNotFoundException {
+        return new FileOutputStream(new File(testDirectory + filename));
     }
 }
