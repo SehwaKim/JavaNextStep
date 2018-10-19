f70d8d5bc73518ce9a662519fd1715d2b30af675
SehwaKim <sehwakm@gmail.com>
2018-10-19 17:42:53 +0900

    RequestLine 클래스가 요청라인만 따로 파싱해서 갖고있도록 구현하고 그 테스트코드를 작성

---
+++ study01/web-application-server/src/test/java/http/RequestLineTest.java
@@ -0,0 +1,25 @@
+package http;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RequestLineTest {
+    @Test
+    public void create_method() {
+        RequestLine line = new RequestLine("GET /index.html HTTP/1.1");
+        assertThat(line.getMethod()).isEqualTo("GET");
+        assertThat(line.getPath()).isEqualTo("/index.html");
+
+        line = new RequestLine("POST /index.html HTTP/1.1");
+        assertThat(line.getPath()).isEqualTo("/index.html");
+    }
+
+    @Test
+    public void create_path_and_params() {
+        RequestLine line = new RequestLine("GET /user/create?userId=dvno&password=1235 HTTP/1.1");
+        assertThat(line.getMethod()).isEqualTo("GET");
+        assertThat(line.getPath()).isEqualTo("/user/create");
+        assertThat(line.getParameter("userId")).isEqualTo("dvno");
+    }
+}
